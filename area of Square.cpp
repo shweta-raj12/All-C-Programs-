974ff221b49a8c72c8e6c0ed2c8c1b745bd15418
Shweta Raj <shwetaraj532@gmail.com>
2025-01-04 20:10:55 +0530

    Add files via upload

---
+++ area of Square.cpp
@@ -0,0 +1,14 @@
+//Program to Calculae Areaa of Circle
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int s, ar;
+    cout << "Enter Side of Circle : " << endl;
+    cin >> s;
+    ar=s*s;
+    cout << "Area of Circle is : "<<ar;
+    return 0;
+}
