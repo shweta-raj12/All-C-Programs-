4606d1017bc98d7bfacc6003457a2a1fbc307bce
Shweta Raj <shwetaraj532@gmail.com>
2025-01-04 19:49:12 +0530

    Add files via upload

---
+++ area of circle.cpp
@@ -0,0 +1,14 @@
+//Program to Calculae Areaa of Circle
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float r, ar;
+    cout << "Enter Radius of Circle : " << endl;
+    cin >> r;
+    ar=3.14*r*r;
+    cout << "Area of Circle is : "<<ar;
+    return 0;
+}
