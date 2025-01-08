10fd3bfb787b947fdbb7799fc8a095cce6515688
Shweta Raj <shwetaraj532@gmail.com>
2025-01-08 22:11:31 +0530

    Add files via upload

---
+++ typecasting program.cpp
@@ -0,0 +1,14 @@
+//TYPECASTING PROGRAM 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x; 
+    cout << "Enter the value of x: " << endl;
+    cin >> x; 
+    float y=(float) x; 
+    cout << y/2;
+    return 0;
+}
