00faaaae3a145bda8c0fd0a82e17545edbeec0c3
Shweta Raj <shwetaraj532@gmail.com>
2025-01-13 22:27:45 +0530

    Add files via upload

---
+++ print a word five times.cpp
@@ -0,0 +1,17 @@
+//PRINT A WORD FIVE TIMES IN C++
+
+// CODE BY SHWETA RAJ
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=0; i<5; i++)
+    {
+        cout << "Hello Guys! " <<endl;
+    }
+    return 0;
+}
+
+// CODE BY SHWETA RAJ
