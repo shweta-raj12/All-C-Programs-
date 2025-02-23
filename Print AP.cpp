848b80faaf99ee4e9cc77da537d42e2dfe1f7d2b
Shweta Raj <shwetaraj532@gmail.com>
2025-02-23 19:37:29 +0530

    Add files via upload

---
+++ Print AP.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << " Enter n" << endl;
+    cin >> n;
+    int a=1;
+    for(int i=1; i<=n;  i++)
+    {
+        cout << a << endl;
+        a=a+2;
+    }
+
+    return 0;
+}
