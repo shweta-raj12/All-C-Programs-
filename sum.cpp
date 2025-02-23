65b1a023c7b8fb7c78671e69a7659334b07871d6
Shweta Raj <shwetaraj532@gmail.com>
2025-02-23 12:25:27 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout << "Enter the value of x" << endl;
+    cin >> x;
+    cout << "Enter the value of y" << endl;
+    cin >> y;
+    x=x+y;
+    cout <<"The Sum is "<< x<< endl;
+    return 0;
+}
